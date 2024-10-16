8b1a369ceec0b7452b44bb23e04c10f06ba5fb89
Soham <sohamgandhi@live.com>
2024-10-16 19:35:41 -0400

    Added Camera.py class to assist with live camera feed

---
+++ Camera.py
@@ -0,0 +1,30 @@
+"""
+Author: Soham Gandhi
+Date Created: 10/16/2024
+Version: 1.0
+Description: This file will read in the camera feed and return the image
+"""
+
+import cv2
+import numpy as np
+
+class Camera:
+    def __init__(self):
+        self.cap = cv2.VideoCapture(0)
+
+    def get_frame(self) -> np.ndarray:
+        """
+        This function will return the frame from the camera using a generator
+        :return: frame
+        """
+        while True:
+            ret, frame = self.cap.read()
+            if not ret:
+                break
+            else:
+                yield frame
+
+    def release(self):
+        self.cap.release()
+        cv2.destroyAllWindows()
+
