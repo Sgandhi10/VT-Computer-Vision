dcbde5330b1eda56f3878f5c4278b896981e05e7
Anthony Diaz <acd445@vt.edu>
2024-11-23 07:24:18 -0500

    Oragnized some code and files

--- Card_Segmentation.py
+++ Card_Segmentation.py
@@ -1,5 +1,13 @@
+import CardDetection2 as CardDetection
 
+'''
+Method to segment rectangles
+Validates if rect is set of card(s) that would be a player or dealer
+'''
 
-
+#TODO CardDetection2 Somewhat successfully segments with each rect being unique
 class Card_Segmenation:
     pass
+    #TODO use yolo model to validate each rect is a set of playing cards
+    #TODO Is Card_Segmentation responsible for logic to track players between images
+    #or just return segmentation for a image
